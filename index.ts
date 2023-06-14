e999d89cf11da0d306d7350c1cf2f7be0b8fdf55
HieuT23511 <hieut23511@gmail.com>
2023-06-14 10:56:33 +0700

    Helmet

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import helmet from 'helmet';
+
+
+
 const app = express();
 const PORT = 3000;
 
-//cau hinh view / parse / morgan: 
+//cau hinh view / parse / morgan/helmet: 
 app.use(bodyParser.json());
 app.use(morgan("common"));
+app.use(helmet());
 
 //router: 
 app.get('/',(req,res)=>{
